e4065f76ecb54b8a70847a312a50da2a0ca79ea0
mathfuru <mathfuru@gmail.com>
2013-03-19 00:21:57 +0900

    Fix deploy of capistrano

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,27 +1,41 @@
-%w{build-essential libssl-dev make gcc* git vim zsh nginx}.each do |name|
-  package name
+user_name = "ec2-user"
+home_dir = "/home/#{user_name}"
+
+%w{make gcc gcc-c++ git vim zsh nginx}.each do |name|
+  package name do
+    action :install
+  end
+end
+
+directory "/home/#{user_name}/tmp" do
+  action :create
 end
 
 script "install_node_and_npm_from_source" do
   interpreter "bash"
   flags "-e"
-  user "root"
+  user user_name
+  cwd "#{home_dir}/tmp"
   code <<-EOS
     wget http://nodejs.org/dist/#{node[:node][:version]}/node-#{node[:node][:version]}.tar.gz
     tar xvfz node-#{node[:node][:version]}.tar.gz
     cd node-#{node[:node][:version]}
-    ./configure --prefix=~/local
+    ./configure --prefix=#{home_dir}/local
     make
     make install
-    echo "export PATH=$PATH:~/local/bin" >> ~/.bashrc
-    curl https://npmjs.org/install.sh | sh
+    echo "export PATH=$PATH:#{home_dir}/local/bin" >> #{home_dir}/.bashrc
+    PATH=$PATH:#{home_dir}/local/bin curl https://npmjs.org/install.sh | sh
   EOS
-  only_if "test ! -f #{node[:node][:src_cachedir]}/redis-#{node[:node][:version]}.tar.gz"
-  notifies :restart, "service[node-server]"
+  not_if "which node && which npm"
+  notifies :restart, "service[nginx]"
 end
 
 cookbook_file "/etc/yum.repos.d/nginx.repo" do
   source "nginx.repo"
   owner "root"
-  notifies :restart,  "service[redis-server]"
+  notifies :restart, "service[nginx]"
+end
+
+service "nginx" do
+  action [:enable,  :start]
 end
